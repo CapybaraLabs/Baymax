13082f4e55fc5a0ea57611517754ff6900a73e85
Napster <napster@npstr.space>
2018-09-05 16:34:51 +0200

    Add nullability annotations

---
+++ src/main/java/space/npstr/baymax/config/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Created by napster on 05.09.18.
+ */
+@FieldsAreNonNullByDefault
+@ParametersAreNonnullByDefault
+@ReturnTypesAreNonNullByDefault
+package space.npstr.baymax.config;
+
+import space.npstr.annotations.FieldsAreNonNullByDefault;
+import space.npstr.annotations.ParametersAreNonnullByDefault;
+import space.npstr.annotations.ReturnTypesAreNonNullByDefault;
